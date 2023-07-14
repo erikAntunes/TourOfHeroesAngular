1def0a4c713ad6bf92c46159d6a5f8d283ba6e7b
Erik Antunes <erik.antunes-ext@viavarejo.com.br>
2023-07-14 14:24:28 -0300

    ajustando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   rocketTitle = 'Tour of Heroes app is running!';
   headerTitle = 'Bem vindo ao Tour dos Heróis';
-  headerDescription = 'Aqui nesse espaço vamos consumir uma Api da Marvel e disponibilizar um catálogo de hérois em Angular';
+  headerDescription =
+    'Aqui nesse espaço vamos consumir uma Api da Marvel e disponibilizar um catálogo de hérois em Angular';
   footerTitle = 'Documentação Oficial';
   footerDescription = 'Guia completo de desenvolvimento Web';
 }
-
