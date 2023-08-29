12ce3b4ce580e5b4e834891f98e1b232d9bba137
Erik Antunes <erik.antunes-ext@viavarejo.com.br>
2023-08-29 18:30:58 -0300

    adicionando MatButton / MatExpansion e MessagesComponent

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,13 +2,17 @@ import { Injectable } from '@angular/core';
 import { HEROES } from './mock-heroes';
 import { Hero } from './hero.model';
 import { Observable, of } from 'rxjs';
+import { MessagesService } from './messages.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
+  constructor(private messagesService: MessagesService) {}
+
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
+    this.messagesService.add('HeroService: fetched heroes');
     return heroes;
   }
 }
