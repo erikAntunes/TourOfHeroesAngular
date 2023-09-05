a20a2382f990acb61deb286832dcf932383ce7a1
Erik Antunes <erik.antunes-ext@viavarejo.com.br>
2023-09-05 16:26:02 -0300

    adicionando nome do heroi na lista

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -27,6 +27,6 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-    this.messagesService.add(`HeroesComponent: Selected hero id=${ hero.id}`);
+    this.messagesService.add(`${hero.name}: Selected hero id=${ hero.id}`);
   }
 }
