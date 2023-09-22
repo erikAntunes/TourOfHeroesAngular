e782287af5aa3d84f1bcdfa3423f1cbf81de7940
Erik Antunes <erik.antunes-ext@viavarejo.com.br>
2023-09-22 14:26:08 -0300

    ajustando bug do  SCSS do Heroes Component

--- src/app/core/components/messages/messages.component.ts
+++ src/app/core/components/messages/messages.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.css'],
+  styleUrls: ['./messages.component.scss'],
 })
 export class MessagesComponent {
   constructor(public messagesService: MessagesService) {}
